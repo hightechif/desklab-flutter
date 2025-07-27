61c9613ab0453fe60a4831ddbe47c87704136017
R. Fadhil <ridhanfadhilah@gmail.com>
2025-07-27 08:54:59 +0700

    FEAT: Add unimported calendar_event model and add activity repository

--- lib/data/repositories/activity_repository.dart
+++ lib/data/repositories/activity_repository.dart
@@ -7,27 +7,54 @@ class ActivityRepository {
   Map<int, List<Activity>> getInitialActivities() {
     return {
       0: [
-        // Monday
+        // Monday, July 14
         Activity(
-          project: 'EDTS-ADMIN-ADMIN-TRAINING',
-          activityName: 'Sharing Session',
-          hours: 5,
-          color: Colors.brown,
+          project: 'KLIK-CORP-KLIK-DEVELOPMENT',
+          activityName: 'Enhancement',
+          hours: 4,
+          color: const Color(0xFF546E7A),
         ),
         Activity(
           project: 'KLIK-CORP-KLIK-DEVELOPMENT',
           activityName: 'Development',
           hours: 4,
-          color: const Color(0xFF4A6572),
+          color: const Color(0xFF546E7A),
+        ),
+      ],
+      1: [
+        // Tuesday, July 15
+        Activity(
+          project: 'EDTS-ADMIN-ADMIN-TRAINING',
+          activityName: 'Sharing Session',
+          hours: 8,
+          color: Colors.brown,
         ),
       ],
       2: [
-        // Wednesday
+        // Wednesday, July 16
+        Activity(
+          project: 'GURIH-CORP-GURIHMART-DEV',
+          activityName: 'Bugs Fixing',
+          hours: 8,
+          color: const Color(0xFF62727b),
+        ),
+      ],
+      3: [
+        // Thursday, July 17
         Activity(
           project: 'EDTS-ADMIN-ADMIN-LEAVE',
           activityName: 'Annual Leave',
-          hours: 4,
+          hours: 8,
           color: Colors.purple,
+        ),
+      ],
+      4: [
+        // Friday, July 18
+        Activity(
+          project: 'KLIK-CORP-KLIK-DEVELOPMENT',
+          activityName: 'Research / Planning',
+          hours: 8,
+          color: const Color(0xFF4A6572),
         ),
       ],
     };
